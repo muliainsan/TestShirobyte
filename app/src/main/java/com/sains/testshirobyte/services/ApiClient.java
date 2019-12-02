0c62b691c6b82bbc21acd359699e4ee6575e5b88
insanmulia <insanmuliaisa@gmail.com>
2019-12-02 20:30:16 +0700

    fix

--- app/src/main/java/com/sains/testshirobyte/services/ApiClient.java
+++ app/src/main/java/com/sains/testshirobyte/services/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    public static final String BASE_URL = "https://test.shirobyte.com/API/";
+    public static final String BASE_URL = "http://test.shirobyte.com/API/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient(){
--- app/src/main/java/com/sains/testshirobyte/services/ApiInterface.java
+++ app/src/main/java/com/sains/testshirobyte/services/ApiInterface.java
@@ -2,10 +2,9 @@ package com.sains.testshirobyte.services;
 import com.sains.testshirobyte.Data;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Query;
 
 public interface ApiInterface {
-    @GET("API/list.php")
+    @GET("list.php")
     Call<Data> getData();
 
 
